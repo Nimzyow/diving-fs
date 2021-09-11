e256cbc38a269d9dea3224ccf7b5c0b194d03408
Nima Soufiani <>
2021-09-11 11:59:16 +0100

    create context in seperate file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,22 +2,19 @@ import { ApolloServer, gql } from "apollo-server"
 import * as path from "path"
 import * as types from "./schema/index"
 import { makeSchema } from "nexus"
-import { db } from "./db"
-import { PrismaClient } from "@prisma/client"
-
-interface Context {
-    db: PrismaClient
-}
-
-const context = {
-    db,
-}
+import { context } from "./context"
 
 const schema = makeSchema({
     types,
     outputs: {
         schema: path.join(__dirname, "/generated/schema.graphql"),
         typegen: path.join(__dirname + "/generated/nexus.ts"),
+    },
+    contextType: {
+        // Path to the module where the context type is exported
+        module: path.join(__dirname, "./context.ts"),
+        // Name of the export in that module
+        export: "Context",
     },
 })
 
