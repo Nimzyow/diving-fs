f01dc405deaaaf04aab363fbd100b03eed7780b8
Nima Soufiani <>
2021-09-11 19:38:36 +0100

    query user resolve

--- backend/src/schema/query.ts
+++ backend/src/schema/query.ts
@@ -13,8 +13,17 @@ export const Query = objectType({
             type: "User",
             resolve: async (parent, args, context) => {
                 // return { id: "1", firstName: "Nima", lastName: "adsad", email: "n_soufiani@hotmail.com" }
-                const user = await context.prisma.user.findFirst()
-                return user
+                try {
+                    const result = context.prisma.user.findUnique({
+                        where: {
+                            id: 1,
+                        },
+                    })
+                    return result
+                } catch (error) {
+                    console.log(error)
+                }
+                return null
             },
         })
     },
