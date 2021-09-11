3af9220267b055c673769024a5476464f6bae37e
Nima Soufiani <>
2021-09-11 11:49:14 +0100

    make nexus schema and refactor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,43 @@
 import { ApolloServer, gql } from "apollo-server"
-
+import * as path from "path"
 import * as types from "./schema/index"
+import { makeSchema } from "nexus"
 
-const books = [
-    {
-        title: "The Awakening",
-        author: "Kate Chopin",
-    },
-    {
-        title: "City of Glass",
-        author: "Paul Auster",
-    },
-]
+// const books = [
+//     {
+//         title: "The Awakening",
+//         author: "Kate Chopin",
+//     },
+//     {
+//         title: "City of Glass",
+//         author: "Paul Auster",
+//     },
+// ]
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`
+// const typeDefs = gql`
+//     type Query {
+//         hello: String
+//     }
+// `
 
-const resolvers = {
-    Query: {
-        hello: () => "world",
+// const resolvers = {
+//     Query: {
+//         hello: () => "world",
+//     },
+// }
+
+const schema = makeSchema({
+    types,
+    outputs: {
+        schema: path.join(__dirname, "/generated/schema.graphql"),
+        typegen: path.join(__dirname + "/generated/nexus.ts"),
     },
-}
+})
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ schema })
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
