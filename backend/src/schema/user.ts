e0c862e7849378b4f94cbe87ca3e8414f7d7ffe0
Nima Soufiani <>
2021-09-11 19:37:04 +0100

    user id type def

--- backend/src/schema/user.ts
+++ backend/src/schema/user.ts
@@ -3,7 +3,7 @@ import { objectType } from "nexus"
 export const User = objectType({
     name: "User",
     definition(t) {
-        t.nonNull.string("id"),
+        t.nonNull.id("id"),
             t.nonNull.string("firstName"),
             t.nonNull.string("lastName"),
             t.nonNull.string("email")
