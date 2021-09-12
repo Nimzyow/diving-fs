41e1c6120ac9dd341425ce7e2a82a9290e2a5347
Nima Soufiani <>
2021-09-12 23:38:08 +0100

    create createUser mutation

---
+++ backend/src/schema/mutation.ts
@@ -0,0 +1,61 @@
+import { extendType, stringArg } from "nexus"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+
+export const Mutation = extendType({
+    type: "Mutation",
+    definition(t) {
+        t.field("createUser", {
+            type: "Token",
+            args: {
+                firstName: stringArg(),
+                lastName: stringArg(),
+                email: stringArg(),
+                password: stringArg(),
+            },
+            resolve: async (parent, args, context) => {
+                // 1) user needs to send inputs: firstName, lastName, email, password
+                // 2) encrypt the password
+                // 3) create the user in the db
+                // 4) if successful, return token
+                const { email, lastName, firstName, password } = args
+
+                if (email && lastName && firstName && password) {
+                    try {
+                        const salt = await bcrypt.genSalt(10)
+
+                        const hashedPassword = await bcrypt.hash(password, salt)
+
+                        const user = await context.prisma.user.upsert({
+                            where: {
+                                email,
+                            },
+                            update: {},
+                            create: {
+                                email,
+                                firstName,
+                                lastName,
+                                password: hashedPassword,
+                            },
+                        })
+
+                        const payload = {
+                            user: {
+                                id: user.id,
+                            },
+                        }
+
+                        return {
+                            token: jwt.sign(payload, process.env.JWTSECRET || "", { expiresIn: 360000 }),
+                        }
+                    } catch (error) {
+                        console.log(error)
+                        return null
+                    }
+                }
+
+                return null
+            },
+        })
+    },
+})
