2b90a43f2aa8a5c1e2f76ea10f8a68747ebfd5eb
Nima Soufiani <>
2021-09-12 12:53:21 +0100

    add user field to context object

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -1,10 +1,20 @@
 import prisma from "./db"
 import { PrismaClient } from "@prisma/client"
 
+interface User {
+    id: string
+    firstName: string
+    lastName: string
+    email: string
+    isSuperUser: boolean
+}
+
 export interface Context {
     prisma: PrismaClient
+    user: User | null
 }
 
 export const context = {
     prisma,
+    user: null,
 }
