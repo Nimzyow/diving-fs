cb92f05eb7a3618c6fea62af30777876aba0bf31
Nima Soufiani <>
2021-09-14 22:47:09 +0100

    add succesful test

--- backend/tests/Users.test.ts
+++ backend/tests/Users.test.ts
@@ -3,13 +3,12 @@ import jwt from "jsonwebtoken"
 
 const ctx = createTestContext()
 
-jest.mock("jsonwebtoken")
-
 it("ensures that a user can be created", async () => {
     const sign = jest.spyOn(jwt, "sign")
     sign.mockImplementation(() => () => "signed")
 
-    // Create a new draft
+    // ! define return type and send it as a generic inside request
+    // Create a new user
     const createUserResult = await ctx.client.request(`            # 1
     mutation Mutation {
       createUser(firstName: "Nima", lastName: "Soufiani", email: "nima@example.com", password: "Randompasswordman") {
@@ -21,13 +20,15 @@ it("ensures that a user can be created", async () => {
       }
     }
   `)
-    // Snapshot that create user and expect `published` to be false
+    console.log(createUserResult)
+    // Snapshot that create user
     expect(createUserResult).toMatchInlineSnapshot(`
-Object {
-  "createUser": Object {
-    "errors": Array [],
-    "token": "signed",
-  },
-}
-`) // 3
+      Object {
+        "createUser": Object {
+          "errors": Array [],
+          "token": "signed",
+        },
+      }
+      `) // 3
+    expect(createUserResult).toEqual({ createUser: { token: "signed", errors: [] } })
 })
