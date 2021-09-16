d70518db3db3c99eda87653113e99cd1bb1f08da
Nima Soufiani <>
2021-09-16 21:41:38 +0100

    packages installed for future apollo with express release due to sub type

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -40,3 +40,88 @@ export const server = new ApolloServer({
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`)
 })
+
+// with apollo express
+/**
+ * import { ApolloServer } from "apollo-server-express"
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core"
+import express from "express"
+import http from "http"
+import { nexusPrisma } from "nexus-plugin-prisma"
+import * as path from "path"
+import * as types from "./schema/index"
+import { makeSchema } from "nexus"
+import { Context, getUser } from "./context"
+import prisma from "./db"
+
+export const schema = makeSchema({
+    types,
+    outputs: {
+        schema: path.join(__dirname, "/generated/schema.graphql"),
+        typegen: path.join(__dirname + "/generated/nexus.ts"),
+    },
+    contextType: {
+        module: path.join(__dirname, "./context.ts"),
+        export: "Context",
+    },
+    plugins: [nexusPrisma()],
+    sourceTypes: {
+        modules: [
+            {
+                module: "@prisma/client",
+                alias: "prisma",
+            },
+        ],
+    },
+})
+
+async function startApolloServer() {
+    // Required logic for integrating with Express
+    const app = express()
+    const httpServer = http.createServer(app)
+
+    // Same ApolloServer initialization as before, plus the drain plugin.
+    const server = new ApolloServer({
+        schema,
+        context: async ({ req }: { req: { headers: { authorization: string } } }): Promise<Context> => {
+            return {
+                prisma: prisma,
+                user: await getUser(req.headers.authorization, prisma),
+            }
+        },
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    })
+
+    // More required logic for integrating with Express
+    await server.start()
+    server.applyMiddleware({
+        app,
+
+        // By default, apollo-server hosts its GraphQL endpoint at the
+        // server root. However, *other* Apollo Server packages host it at
+        // /graphql. Optionally provide this to match apollo-server.
+        path: "/",
+    })
+
+    // Modified server startup
+    await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+}
+
+startApolloServer()
+
+// export const server = new ApolloServer({
+//     schema,
+//     context: async ({ req }: { req: { headers: { authorization: string } } }): Promise<Context> => {
+//         return {
+//             prisma: prisma,
+//             user: await getUser(req.headers.authorization, prisma),
+//         }
+//     },
+// })
+
+// server.listen().then(({ url }) => {
+//     console.log(`🚀  Server ready at ${url}`)
+// })
+
+ */
--- backend/tests/Users.test.ts
+++ backend/tests/Users.test.ts
@@ -3,31 +3,59 @@ import jwt from "jsonwebtoken"
 
 const ctx = createTestContext()
 
-it("ensures that a user can be created", async () => {
-    const sign = jest.spyOn(jwt, "sign")
-    sign.mockImplementation(() => () => "signed")
+describe("User", () => {
+    it("can be created", async () => {
+        const sign = jest.spyOn(jwt, "sign")
+        sign.mockImplementation(() => () => "signed")
 
-    // ! define return type and send it as a generic inside request
-    // Create a new user
-    const createUserResult = await ctx.client.request(`            # 1
-    mutation Mutation {
-      createUser(firstName: "Nima", lastName: "Soufiani", email: "nima@example.com", password: "Randompasswordman") {
-        token
-        errors {
-          message
-          code
+        // ! define return type and send it as a generic inside request
+        // Create a new user
+        const createUserResult = await ctx.client.request(`            # 1
+      mutation Mutation {
+        createUser(firstName: "Nima", lastName: "Soufiani", email: "nima@example.com", password: "Randompasswordman") {
+          token
+          errors {
+            message
+            code
+          }
         }
       }
-    }
-  `)
-    // Snapshot that create user
-    expect(createUserResult).toMatchInlineSnapshot(`
-      Object {
-        "createUser": Object {
-          "errors": Array [],
-          "token": "signed",
-        },
-      }
-      `) // 3
-    expect(createUserResult).toEqual({ createUser: { token: "signed", errors: [] } })
+    `)
+        // Snapshot that create user
+        expect(createUserResult).toMatchInlineSnapshot(`
+        Object {
+          "createUser": Object {
+            "errors": Array [],
+            "token": "signed",
+          },
+        }
+        `) // 3
+        expect(createUserResult).toEqual({ createUser: { token: "signed", errors: [] } })
+    })
+
+    it("will throw error with no email, firstName, lastName or password", async () => {
+        const sign = jest.spyOn(jwt, "sign")
+        sign.mockImplementation(() => () => "signed")
+
+        // ! define return type and send it as a generic inside request
+        // Create a new user
+        const createUserResult = await ctx.client.request(`            # 1
+          mutation Mutation {
+            createUser(firstName: "", lastName: "", email: "", password: "") {
+              token
+              errors {
+                message
+                code
+              }
+            }
+          }
+        `)
+
+        expect(createUserResult).toEqual({
+            createUser: {
+                token: "signed",
+                errors: [{ code: "INVALID_INPUTS", message: "Please enter valid inputs" }],
+            },
+        })
+    })
 })
