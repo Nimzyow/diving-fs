0e69ed33f171c5629f5423fb06934349b1877fd2
Nima Soufiani <>
2021-09-17 23:15:10 +0100

    amend test so that it compatiable with apollo client 3

--- backend/tests/Users.test.ts
+++ backend/tests/Users.test.ts
@@ -1,61 +1,82 @@
-import { createTestContext } from "./__helpers"
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import { ApolloServer, gql } from "apollo-server";
+import { schema } from "../src/server";
+import prisma from "../src/db";
 
-const ctx = createTestContext()
+const CREATE_USER = gql`
+  mutation Mutation(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    createUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      errors {
+        message
+        code
+      }
+    }
+  }
+`;
+
+const server = new ApolloServer({
+  schema,
+  context: {
+    prisma: prisma,
+  },
+});
 
 describe("User", () => {
-    it("can be created", async () => {
-        const sign = jest.spyOn(jwt, "sign")
-        sign.mockImplementation(() => () => "signed")
+  it("can be created", async () => {
+    const sign = jest.spyOn(jwt, "sign");
+    sign.mockImplementation(() => () => "signed");
 
-        // ! define return type and send it as a generic inside request
-        // Create a new user
-        const createUserResult = await ctx.client.request(`            # 1
-      mutation Mutation {
-        createUser(firstName: "Nima", lastName: "Soufiani", email: "nima@example.com", password: "Randompasswordman") {
-          token
-          errors {
-            message
-            code
-          }
-        }
-      }
-    `)
-        // Snapshot that create user
-        expect(createUserResult).toMatchInlineSnapshot(`
-        Object {
-          "createUser": Object {
-            "errors": Array [],
-            "token": "signed",
-          },
-        }
-        `) // 3
-        expect(createUserResult).toEqual({ createUser: { token: "signed", errors: [] } })
-    })
+    // ! define return type and send it as a generic inside request
+    // Create a new user
+    const createUserResult = await server.executeOperation({
+      query: CREATE_USER,
+      variables: {
+        firstName: "Nima",
+        lastName: "Soufiani",
+        email: "nima@example.com",
+        password: "Randompasswordman",
+      },
+    });
 
-    it("will throw error with no email, firstName, lastName or password", async () => {
-        const sign = jest.spyOn(jwt, "sign")
-        sign.mockImplementation(() => () => "signed")
+    expect(createUserResult.data).toEqual({
+      createUser: { token: "signed", errors: [] },
+    });
+  });
 
-        // ! define return type and send it as a generic inside request
-        // Create a new user
-        const createUserResult = await ctx.client.request(`            # 1
-          mutation Mutation {
-            createUser(firstName: "", lastName: "", email: "", password: "") {
-              token
-              errors {
-                message
-                code
-              }
-            }
-          }
-        `)
+  it("will throw error with no email, firstName, lastName or password", async () => {
+    const sign = jest.spyOn(jwt, "sign");
+    sign.mockImplementation(() => () => "signed");
 
-        expect(createUserResult).toEqual({
-            createUser: {
-                token: "signed",
-                errors: [{ code: "INVALID_INPUTS", message: "Please enter valid inputs" }],
-            },
-        })
-    })
-})
+    // ! define return type and send it as a generic inside request
+    // Create a new user
+    const createUserResult = await server.executeOperation({
+      query: CREATE_USER,
+      variables: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+    });
+
+    expect(createUserResult.data).toEqual({
+      createUser: {
+        token: null,
+        errors: [
+          { code: "INVALID_INPUTS", message: "Please enter valid inputs" },
+        ],
+      },
+    });
+  });
+});
--- backend/tests/__helpers.ts
+++ backend/tests/__helpers.ts
@@ -1,45 +1,45 @@
-import { ApolloServer, ServerInfo } from "apollo-server"
-import getPort, { makeRange } from "get-port"
-import { GraphQLClient } from "graphql-request"
-import prisma from "../src/db"
-import { schema } from "../src/server"
-type TestContext = {
-    client: GraphQLClient
-}
-export function createTestContext(): TestContext {
-    let ctx = {} as TestContext
-    const graphqlCtx = graphqlTestContext()
-    beforeEach(async () => {
-        // 2
-        const client = await graphqlCtx.before()
-        Object.assign(ctx, {
-            client,
-        })
-    })
-    afterEach(async () => {
-        // 3
-        await graphqlCtx.after()
-    })
-    return ctx // 8
-}
+// import { ApolloServer, ServerInfo } from "apollo-server";
+// import getPort, { makeRange } from "get-port";
+// import { GraphQLClient } from "graphql-request";
+// import prisma from "../src/db";
+// import { schema } from "../src/server";
+// type TestContext = {
+//   client: GraphQLClient;
+// };
+// export function createTestContext(): TestContext {
+//   let ctx = {} as TestContext;
+//   const graphqlCtx = graphqlTestContext();
+//   beforeEach(async () => {
+//     // 2
+//     const client = await graphqlCtx.before();
+//     Object.assign(ctx, {
+//       client,
+//     });
+//   });
+//   afterEach(async () => {
+//     // 3
+//     await graphqlCtx.after();
+//   });
+//   return ctx; // 8
+// }
 
-const server = new ApolloServer({
-    schema,
-    context: {
-        prisma: prisma,
-    },
-})
+// const server = new ApolloServer({
+//   schema,
+//   context: {
+//     prisma: prisma,
+//   },
+// });
 
-function graphqlTestContext() {
-    let serverInstance: ServerInfo | null = null
-    return {
-        async before() {
-            const port = await getPort({ port: makeRange(4000, 6000) }) // 4
-            serverInstance = await server.listen({ port }) // 5
-            return new GraphQLClient(`http://localhost:${port}`) // 6
-        },
-        async after() {
-            serverInstance?.server.close() // 7
-        },
-    }
-}
+// function graphqlTestContext() {
+//   let serverInstance: ServerInfo | null = null;
+//   return {
+//     async before() {
+//       const port = await getPort({ port: makeRange(4000, 6000) }); // 4
+//       serverInstance = await server.listen({ port }); // 5
+//       return new GraphQLClient(`http://localhost:${port}`); // 6
+//     },
+//     async after() {
+//       serverInstance?.server.close(); // 7
+//     },
+//   };
+// }
