9e9eac4108a61cac7b4354c1ba5ab86bc63106ba
Nima Soufiani <>
2021-09-19 09:28:31 +0100

    create .graphql file and add placeholder pages

---
+++ frontend/src/pages/account/AccountContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AccountContainer = () => {
+    return (
+        <div>
+            <p>Account container</p>
+        </div>
+    )
+}
+
+export default AccountContainer
