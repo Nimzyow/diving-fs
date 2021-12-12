69e28d69d4ac99f183bf09e9661b33262ce1d651
Nima Soufiani <>
2021-12-12 20:22:46 +0000

    cypress gain access to the input and type in dummy text

--- frontend/cypress/integration/sample_spec.ts
+++ frontend/cypress/integration/sample_spec.ts
@@ -1,5 +1,12 @@
 describe("This is my first test", () => {
-    it("Does not do too much", () => {
-        expect(true).to.equal(true)
+    it("visits the login page", () => {
+        cy.visit("http://localhost:3000/")
+
+        cy.contains("Log in")
+        cy.contains("Register here").click()
+        cy.contains("Register")
+        cy.contains("Login here").click()
+        cy.get("input[type=email]").type("test@example.com")
+        cy.get("input[type=password]").type("somethingOrAnother")
     })
 })
