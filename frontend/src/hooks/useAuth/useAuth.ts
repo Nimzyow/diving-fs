6104ed3c0e553f80c5ae083965116998bbb9a858
Nima Soufiani <>
2021-12-12 14:51:29 +0000

    create logout function

--- frontend/src/hooks/useAuth/useAuth.ts
+++ frontend/src/hooks/useAuth/useAuth.ts
@@ -1,12 +1,21 @@
+import { useApolloClient } from "@apollo/client"
+
 import { useMeQuery } from "../../generated/graphql"
 
 export const useAuth = () => {
+    const client = useApolloClient()
     const { data: userData, loading: userLoading, error: userError, refetch: userRefetch } = useMeQuery()
+
+    const logout = () => {
+        localStorage.removeItem("token")
+        client.resetStore()
+    }
 
     return {
         userData: userData?.me,
         userError,
         userLoading,
         userRefetch,
+        logout,
     }
 }
