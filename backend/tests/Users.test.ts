6c585c7352a58f15dbc8f9328d433ca50df1c354
Nima Soufiani <>
2021-12-16 15:49:45 +0000

    add test for getting logged in user

--- backend/tests/Users.test.ts
+++ backend/tests/Users.test.ts
@@ -30,8 +30,19 @@ const CREATE_USER = gql`
     }
 `
 
-describe("User", () => {
-    it("can be created", async () => {
+const ME_QUERY = gql`
+    query Me {
+        me {
+            id
+            firstName
+            lastName
+            email
+        }
+    }
+`
+
+describe("Create user mutation", () => {
+    it("is successful", async () => {
         const sign = jest.spyOn(jwt, "sign")
         sign.mockImplementation(() => () => "signed")
 
@@ -104,7 +115,10 @@ describe("User", () => {
             },
         })
     })
-    it("can log in", async () => {
+})
+
+describe("login mutation", () => {
+    it("is successful", async () => {
         const sign = jest.spyOn(jwt, "sign")
         sign.mockImplementation(() => "signed")
 
@@ -194,6 +208,67 @@ describe("User", () => {
                     { code: "INVALID_CREDENTIALS", message: "Please check your email and password" },
                 ],
             },
+        })
+    })
+})
+
+describe("me query", () => {
+    it("successfully queries logged in user", async () => {
+        const server = new ApolloServer({
+            schema,
+            context: {
+                prisma: prismaMock,
+                user: {
+                    id: "1",
+                },
+            },
+        })
+
+        const dateNow = new Date()
+
+        const user: User = {
+            id: "1",
+            firstName: "Rich",
+            lastName: "asdsads",
+            email: "adasd@asdas.com",
+            password: "asdasdasdas",
+            isSuperUser: false,
+            role: "USER",
+            status: "ACTIVE",
+            createdAt: dateNow,
+            updatedAt: dateNow,
+        }
+
+        prismaMock.user.findUnique.mockResolvedValue(user)
+
+        const loginUserResult = await server.executeOperation({
+            query: ME_QUERY,
+        })
+
+        expect(loginUserResult.data).toEqual({
+            me: {
+                id: "1",
+                firstName: "Rich",
+                lastName: "asdsads",
+                email: "adasd@asdas.com",
+            },
+        })
+    })
+    it("returns null for user not logged in", async () => {
+        const server = new ApolloServer({
+            schema,
+            context: {
+                prisma: prismaMock,
+                user: null,
+            },
+        })
+
+        const loginUserResult = await server.executeOperation({
+            query: ME_QUERY,
+        })
+
+        expect(loginUserResult.data).toEqual({
+            me: null,
         })
     })
 })
