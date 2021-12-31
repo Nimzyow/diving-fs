6eb2a1a77f40de127f67806772f8a0089af79a97
Nima Soufiani <n_soufiani@hotmail.com>
2021-12-31 16:30:27 +0000

    add Login test from iPad

---
+++ frontend/src/pages/account/components/Login.test.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import {fireEvent, render} from "@testing-library/react"
+
+import {Login} from "./Login"
+
+describe("Login", () => {
+    test("Should display not an email validation error", () => {
+        // const {getByPlaceholderText, getByText} = render(
+        //     <Login />
+        // )
+        //     fireEvent.change(getByPlaceholderText("Enter email"), {target: {value: "test"}})
+        //     fireEvent.change(getByPlaceholderText("Password"), {target: {value: "testPassword"}})
+
+        //     const emailValidationError = getByText("email has been taken")
+
+        //     expect(emailValidationError).toBeDefined()
+    })
+})
